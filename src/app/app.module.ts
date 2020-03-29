f9fe3e81604e24d32c62758c496b330d447cfeb0
nishchay7pixels <nishchay.verma.7pixels@gmail.com>
2020-03-30 03:45:36 +0530

    Added Charts and Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { GraphDashboardComponent } from './Components/dashboard/graph-dashboard/graph-dashboard.component';
+import {ChartsModule} from "ng2-charts";
+import {ClarityModule} from "@clr/angular";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GraphDashboardComponent,
   ],
+  //using ng2-charts
   imports: [
+    ChartsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
